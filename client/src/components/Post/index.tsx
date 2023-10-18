7fe0c8a8c4fd9a39c5f72258083b103c0ddfe6eb
Mohamed Bensaleh <mobensaleh@outlook.com>
2023-10-17 18:42:03 -0600

    added read more component

--- client/src/components/Post/index.tsx
+++ client/src/components/Post/index.tsx
@@ -1,19 +1,29 @@
 import { Card, CardContent, Typography, Button, Link, CardActionArea } from "@mui/material";
 import styles from '../../styles/Post.module.scss';
-
 import { PostProps } from "./types";
 
-const Post: React.FC<PostProps> = ({ categoryType, title, description = "", externalLink, url }) => {
+// @ts-ignore
+import ReadMoreAndLess from 'react-read-more-less';
 
+
+const Post: React.FC<PostProps> = ({ categoryType, title, description = "", externalLink, url }) => {
+  var desc = description === null ? "" : description;
   return (
     <Card className={styles.card} sx={{ maxWidth: 1000 }}>
       <CardActionArea>
         <CardContent>
           <Typography className={styles.title} component="p">{title}</Typography>
-          <h2 className={styles.description}>
-            {description}
-          </h2>
+          <div className={styles.description}>
+            <ReadMoreAndLess
+                className="read-more-content"
+                readMoreText=" Read more"
+                readLessText=" Read less"
+            >
+                {desc}
+            </ReadMoreAndLess>
 
+          </div>
+        
           {categoryType === "discussion" ? (
               <Button
                   component={Link}
